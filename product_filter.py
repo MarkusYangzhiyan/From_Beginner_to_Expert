9216357f7ed04f5aeb00d7502b6c4d9ff3211a5e
MarkusYangzhiyan <yangzhiyanmarkus@163.com>
2026-06-13 15:09:58 +0800

    实现按产品名称、分类和价格筛选产品

--- product_filter.py
+++ product_filter.py
@@ -7,18 +7,24 @@ products = [
 ]
 
 
-def filter_product(name = "", category = "", max_price = None):
+def filter_product(name="",category="",max_price=None):
+
+    """
+    根据名称、分类和最高价格筛选出产品
+    """
 
     results = []
+
     for product in products:
+
         if name and name not in product["name"]:
             continue 
 
-        if category and category != product['category']:
+        if category and category != product["category"]:
             continue 
-        
-        if max_price is not None and product['price'] > max_price:
-            continue
+
+        if max_price is not None and max_price < product["price"]:
+            continue 
 
         results.append(product)
 
@@ -32,3 +38,43 @@ def show_products(results):
         return 
     
     print("\n 筛选结果")
+
+    for product in results:
+        print(
+            f"产品名称：{product["name"]}",
+            f"产品分类：{product["category"]}",
+            f"最高价格：{product['price']}"
+        )
+
+
+
+while True:
+
+    print("\n == 产品筛选器 == ")
+    print("直接回车表示不限制条件，输入 q 退出程序")
+
+    name = input("产品名称：").strip()
+
+    if name.lower() == "q":
+        print("程序已退出")
+        break 
+
+    category = input("产品类型：").strip()
+    price_text = input("最高价格：").strip()
+
+    if price_text:
+        try:
+            max_price = float(price_text)
+        except ValueError:
+            print("价格必须是数字，请重新输入。")
+            continue 
+    else:
+        max_price = None
+
+
+    matched_products = filter_product(
+        name = name,
+        category=category,
+        max_price=max_price
+    )
+    show_products(matched_products)
